f81a120b2b460af02591ae6fa5c2c7f9f82e7734
Joris Dewaele <joris.dewaele@tutanota.com>
2020-01-16 23:01:30 +0100

    Routing exercise

--- src/index.js
+++ src/index.js
@@ -1,41 +1,35 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
+import uuidv4 from 'uuid/v4';
+
+import models from './models';
 
 const app = express();
 
 // Domains to whitelist in production(!!!), see Node.js CORS middleware
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-let users = {
-  1: {
-    id: '1',
-    username: 'Robin Wieruch',
-  },
-  2: {
-    id: '2',
-    username: 'Dave Davids',
-  },
-};
-let messages = {
-  1: {
-    id: '1',
-    text: 'Hello World',
-    userId: '1',
-  },
-  2: {
-    id: '2',
-    text: 'By World',
-    userId: '2',
-  },
-};
+app.use((req, res, next) => {
+  req.context = {
+    models,
+    me: users[1],
+  };
+  next();
+});
+
+app.get('/session', (req, res) => {
+  return res.send(req.context.models.users[req.context.me.id]);
+});
 
 app.get('/users', (req, res) => {
-  return res.send(Object.values(users));
+  return res.send(Object.values(req.context.models.users));
 });
 
 app.get('/users/:userId', (req, res) => {
-  return res.send(users[req.params.userId]);
+  return res.send(req.context.models.users[req.params.userId]);
 });
 
 app.post('/users', (req, res) => {
@@ -55,12 +49,54 @@ app.delete('/users/:userId', (req, res) => {
 });
 
 app.get('/messages', (req, res) => {
-  return res.send(Object.values(messages));
+  return res.send(Object.values(req.context.models.messages));
 });
 
 app.get('/messages/:messageId', (req, res) => {
-  return res.send(messages[req.params.messageId]);
+  return res.send(req.context.models.messages[req.params.messageId]);
 });
+
+app.post('/messages', (req, res) => {
+  const id = uuidv4();
+  const message = {
+    id,
+    text: req.body.text,
+    userId: req.context.me.id,
+  };
+
+  req.context.models.messages[id] = message;
+
+  return res.send(message);
+});
+
+app.delete('/messages/:messageId', (req, res) => {
+  const {
+      [req.params.messageId]: message,
+      ...otherMessages
+  } = req.context.models.messages;
+
+  req.context.models.messages = otherMessages;
+
+  return res.send(message);
+});
+
+app.put('/messages/:messageId', (req, res) => {
+  const message = {
+    id: req.params.messageId,
+    text: req.body.text,
+    userId: req.me.id,
+  };
+
+  if((messages[message.id].userId) === message.userId) {
+
+    messages[message.id] = message.text;
+
+    return res.send(message);
+  } else {
+    return res.send("Sorry, you are not the creator of the message");
+  }
+});
+
 
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`),
